ee6d2b19ab604eda35d90496b1f898b60e101cb2
Alex <iseldomused@gmail.com>
2017-11-24 01:28:49 +0300

    upd 10

---
+++ 10. while/Summator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator {
+	class Program {
+		static void Main(string[] args) {
+			while (true) {
+				int sum = 0;
+
+				Console.WriteLine("Введите число");
+				int number = int.Parse(Console.ReadLine());
+				sum = sum + number;
+
+				Console.WriteLine("Сумма всех введенных чисел " + sum);
+				Console.WriteLine();
+			}
+		}
+	}
+}
--- 10. while/Summator/Properties/AssemblyInfo.cs
+++ 10. while/Summator/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("327dbd8e-77c4-4233-abfe-2e979da68937")]
+[assembly: Guid("aa35c4f4-1f29-4118-9b65-687b772b8c95")]
 
 // Version information for an assembly consists of the following four values:
 //
